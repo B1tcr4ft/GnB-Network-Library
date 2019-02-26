97fabbec9f73fad024c721da1bc85e121f19cfae
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-02-26 17:02:58 +0100

    Rollback

--- src/util/json-util.js
+++ src/util/json-util.js
@@ -1,87 +1,67 @@
-(function(window) {
-    'use strict';
+import { Network } from '../network/network';
+import * as jsbayes from 'jsbayes'
 
-    import { Network } from '../network/network';
-    import * as jsbayes from 'jsbayes'
+/**
+ * @deprecated
+ * Construct a jsbayes graph over a JSON containing
+ * its definition
+ * @param json {JSON} the json file
+ * @returns {jsbayes} the jsbayes graph
+ */
+function getGraphFromJSON(json) {
+    let graph = jsbayes.newGraph();
 
-    /**
-     * @deprecated
-     * Construct a jsbayes graph over a JSON containing
-     * its definition
-     * @param json {JSON} the json file
-     * @returns {jsbayes} the jsbayes graph
-     */
-    function getGraphFromJSON(json) {
-        let graph = jsbayes.newGraph();
+    //creating the nodes
+    json.nodes.map(node => {
+       let status = node.states.map(state => state.name);
+       graph.addNode(node.id, status);
+    });
 
-        //creating the nodes
-        json.nodes.map(node => {
-            let status = node.states.map(state => state.name);
-            graph.addNode(node.id, status);
-        });
+    //adding data to nodes
+    json.nodes.map(node => {
+        let graphNode = graph.node(node.id);
 
-        //adding data to nodes
-        json.nodes.map(node => {
-            let graphNode = graph.node(node.id);
+        //setting parents
+        node.parents.forEach(parent => graphNode.addParent(graph.node(parent)));
 
-            //setting parents
-            node.parents.forEach(parent => graphNode.addParent(graph.node(parent)));
-
-            //setting cpt
-            let cpt = [];
-            if(node.cpt.length === 1) {
-                cpt = node.cpt[0].map(num => parseFloat(num));
-            } else {
-                node.cpt.map(entry => cpt.push(entry.map(num => parseFloat(num))));
-            }
-
-            graphNode.setCpt(cpt);
-        });
-
-        graph.sample(20000);
-
-        return graph;
-    }
-
-    function defineLib() {
-        let network = {};
-
-        /**
-         * TODO
-         * Get a JSON definition of a network instance
-         * @param network {Network} the network instance
-         * @returns {JSON} the JSON definition
-         */
-        network.getJSONFromNetwork = function(network) {
-            return null;
-        };
-
-        /**
-         * Get a network instance from a JSON containing
-         * its definition
-         * @param json {JSON} the json file
-         * @returns {Network} the network instance
-         */
-        network.getNetworkFromJSON = function(json) {
-            let id = json.id;
-            let name = json.name;
-            let DBWriteName = json.databaseWriteName;
-            let refreshTime = json.refreshTime;
-            let nodes = json.nodes.map(node => Node.fromJSON(node));
-
-            return new Network(id, name, DBWriteName, refreshTime, nodes);
-        }
-    }
-
-    if(typeof module === 'object' && module && typeof module.exports === 'object') {
-        module.exports = defineLib();
-    } else {
-        if(typeof(network) === 'undefined') {
-            window.network = defineLib();
+        //setting cpt
+        let cpt = [];
+        if(node.cpt.length === 1) {
+            cpt = node.cpt[0].map(num => parseFloat(num));
+        } else {
+            node.cpt.map(entry => cpt.push(entry.map(num => parseFloat(num))));
         }
 
-        if(typeof define === 'function' && define.amd) {
-            define('network', [], defineLib());
-        }
-    }
-})(this);
+        graphNode.setCpt(cpt);
+    });
+
+    graph.sample(20000);
+
+    return graph;
+}
+
+/**
+ * TODO
+ * Get a JSON definition of a network instance
+ * @param network {Network} the network instance
+ * @returns {JSON} the JSON definition
+ */
+export function getJSONFromNetwork(network) {
+    return null;
+}
+
+/**
+ * Get a network instance from a JSON containing
+ * its definition
+ * @param json {JSON} the json file
+ * @returns {Network} the network instance
+ */
+export function getNetworkFromJSON(json) {
+    let id = json.id;
+    let name = json.name;
+    let DBWriteName = json.databaseWriteName;
+    let refreshTime = json.refreshTime;
+    let nodes = json.nodes.map(node => Node.fromJSON(node));
+
+    return new Network(id, name, DBWriteName, refreshTime, nodes);
+}
