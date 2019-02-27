c9bd04742093bba7ae7aa30366cbd24469cde922
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-02-27 11:38:53 +0100

    Removed deprecated method

--- src/util/json-util.js
+++ src/util/json-util.js
@@ -1,44 +1,4 @@
 import { Network } from '../network/network';
-import * as jsbayes from 'jsbayes'
-
-/**
- * @deprecated
- * Construct a jsbayes graph over a JSON containing
- * its definition
- * @param json {JSON} the json file
- * @returns {jsbayes} the jsbayes graph
- */
-function getGraphFromJSON(json) {
-    let graph = jsbayes.newGraph();
-
-    //creating the nodes
-    json.nodes.map(node => {
-       let status = node.states.map(state => state.name);
-       graph.addNode(node.id, status);
-    });
-
-    //adding data to nodes
-    json.nodes.map(node => {
-        let graphNode = graph.node(node.id);
-
-        //setting parents
-        node.parents.forEach(parent => graphNode.addParent(graph.node(parent)));
-
-        //setting cpt
-        let cpt = [];
-        if(node.cpt.length === 1) {
-            cpt = node.cpt[0].map(num => parseFloat(num));
-        } else {
-            node.cpt.map(entry => cpt.push(entry.map(num => parseFloat(num))));
-        }
-
-        graphNode.setCpt(cpt);
-    });
-
-    graph.sample(20000);
-
-    return graph;
-}
 
 /**
  * TODO
